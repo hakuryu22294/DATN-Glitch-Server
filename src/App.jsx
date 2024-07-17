360009705f5d4e334e24c1a8606f292036f7cd3a
dthanh0911 <tatu23399@gmail.com>
2024-07-17 14:17:49 +0700

    import Pages

--- src/App.jsx
+++ src/App.jsx
@@ -29,3 +29,25 @@ import {ToastContainer} from 'react-toastify';
 import Sidebar from '@layout/Sidebar';
 import Copyright from '@components/Copyright';
 import AppBar from '@layout/AppBar';
+
+// pages
+const Login  = lazy(() => import('@pages/Login'));
+const SalesAnalytics = lazy(() => import('@pages/SalesAnalytics'));
+const SellersList = lazy(() => import('@pages/SellersList'));
+const SellersTable = lazy(() => import('@pages/SellersTable'));
+const SellersGrid = lazy(() => import('@pages/SellersGrid'));
+const SellerProfile = lazy(() => import('@pages/SellerProfile'));
+const RevenueByPeriod = lazy(() => import('@pages/RevenueByPeriod'));
+const TopProducts = lazy(() => import('@pages/TopProducts'));
+const ProductsGrid = lazy(() => import('@pages/ProductsGrid'));
+const ProductsManagement = lazy(() => import('@pages/ProductsManagement'));
+const ProductEditor = lazy(() => import('@pages/EditProduct'));
+const Banners = lazy(() => import('@pages/Banners'));
+const Orders = lazy(() => import('@pages/Orders'));
+const Statistics = lazy(() => import('@pages/Statistics'));
+const Reviews = lazy(() => import('@pages/Reviews'));
+const Customers = lazy(() => import('@pages/Customers'));
+const Transactions = lazy(() => import('@pages/Transactions'));
+const GeneralSettings = lazy(() => import('@pages/GeneralSettings'));
+const ConnectedApps = lazy(() => import('@pages/ConnectedApps'));
+const PageNotFound = lazy(() => import('@pages/PageNotFound'));
