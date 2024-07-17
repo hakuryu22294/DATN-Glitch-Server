065b42c3b1e70f1f3dacc3be54ae896bcfeefe1b
dthanh0911 <tatu23399@gmail.com>
2024-07-17 14:19:02 +0700

    add file app

--- src/App.jsx
+++ src/App.jsx
@@ -51,3 +51,65 @@ const Transactions = lazy(() => import('@pages/Transactions'));
 const GeneralSettings = lazy(() => import('@pages/GeneralSettings'));
 const ConnectedApps = lazy(() => import('@pages/ConnectedApps'));
 const PageNotFound = lazy(() => import('@pages/PageNotFound'));
+const App = () => {
+    const {width} = useWindowSize();
+    const appRef = useRef(null);
+    const {theme} = useTheme();
+    const path = useLocation().pathname;
+    const withSidebar = path !== '/login' && path !== '/404';
+
+    // Google Analytics init
+    const gaKey = import.meta.env.VITE_GA;
+    gaKey && ReactGA.initialize(gaKey);
+
+    useEffect(() => {
+        appRef.current && appRef.current.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <SidebarProvider>
+            <ThemeProvider theme={{theme: theme}}>
+                <ThemeStyles/>
+                <ToastContainer theme={theme} autoClose={2000} style={{padding: '20px'}}/>
+                {width < 1280 && withSidebar && <AppBar/>}
+                <div className={`app ${!withSidebar ? 'fluid' : ''}`} ref={appRef}>
+                    <ScrollToTop/>
+                    {withSidebar && <Sidebar/>}
+                    <div className="app_content">
+                        {width >= 1280 && withSidebar && <AppBar/>}
+                        <Suspense fallback={<Loader/>}>
+                            <div className="main">
+                                <Routes>
+                                    <Route path="/login" element={<Login/>}/>
+                                    <Route path="/" element={<SalesAnalytics/>}/>
+                                    <Route path="sellers-list" element={<SellersList/>}/>
+                                    <Route path="sellers-table" element={<SellersTable/>}/>
+                                    <Route path="sellers-grid" element={<SellersGrid/>}/>
+                                    <Route path="products-management" element={<ProductsManagement/>}/>
+                                    <Route path="seller-profile" element={<SellerProfile/>}/>
+                                    <Route path="revenue-by-period" element={<RevenueByPeriod/>}/>
+                                    <Route path="top-products" element={<TopProducts/>}/>
+                                    <Route path="products-grid" element={<ProductsGrid/>}/>
+                                    <Route path="product-editor" element={<ProductEditor/>}/>
+                                    <Route path="banners" element={<Banners/>}/>
+                                    <Route path="orders" element={<Orders/>}/>
+                                    <Route path="statistics" element={<Statistics/>}/>
+                                    <Route path="reviews" element={<Reviews/>}/>
+                                    <Route path="customers" element={<Customers/>}/>
+                                    <Route path="transactions" element={<Transactions/>}/>
+                                    <Route path="general-settings" element={<GeneralSettings/>}/>
+                                    <Route path="connected-apps" element={<ConnectedApps/>}/>
+                                    <Route path="*" element={<Navigate to="/404"/>}/>
+                                    <Route path="/404" element={<PageNotFound/>}/>
+                                </Routes>
+                            </div>
+                            {withSidebar && <Copyright/>}
+                        </Suspense>
+                    </div>
+                </div>
+            </ThemeProvider>
+        </SidebarProvider>
+    );
+}
+
+export default App
