bfac6fda1b5c2a10496adcd586e1880e0a6ff87d
Tazuk15 <139194624+Tazuk15@users.noreply.github.com>
2024-07-21 19:09:36 +0700

    Add feature register service

---
+++ src/services/authService/userService.js
@@ -0,0 +1,17 @@
+import { http } from "../../utils/http";
+import { showToastSuccess , showToastError} from "../../config/toastConfig";
+
+const UserService = {
+    RegisterAccount: async (dataRegister) => {
+        try {
+            const { data } = await http.post('/access/shop/sign-up', dataRegister)
+            if(!data) return
+            showToastSuccess(data.message)
+            return data
+        } catch (error) {
+            showToastError(error.response.data.message);
+        }
+    },
+ 
+}
+export default UserService
