25b60f33f994dfba90e805ddef7dea52abd2c005
quangha2805 <114276405+quangha2805@users.noreply.github.com>
2024-07-21 18:31:51 +0700

    Update and fix axios config

--- utils/http.js
+++ utils/http.js
@@ -3,5 +3,8 @@ import axios from 'axios'
 export const http = axios.create({
     baseURL: 'http://localhost:8000/api/v1',
     timeout: 10000,
+     headers: {
+        'x-api-key': 'f171069c37f2e21cc8f3bfa102c0fe8525c0677b775181330170833da3aecd8e32aaf9ec366fe83b6913d80a33e0d8034541fcc6b9fdce7bdbbf769c11da32cf'
+    },
    
 })
