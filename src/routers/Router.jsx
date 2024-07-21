efdf3e9554c3071c6e2bd110371c09d04cf84b81
quangha2805 <114276405+quangha2805@users.noreply.github.com>
2024-07-21 18:13:48 +0700

    Update router feature

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,6 +1,15 @@
+import PathUser from "../config/pathUser/user";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 const UserRouter = [
-
-
+{
+    path:PathUser.NotFound,
+    component: NotFound
+},
+{
+    path:PathUser.Home,
+    component: Home
+}
 ]
 export default UserRouter
