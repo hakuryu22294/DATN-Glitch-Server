a967741ae11d1c49d564661ee0a032a4e90a5a56
Tazuk15 <139194624+Tazuk15@users.noreply.github.com>
2024-07-21 18:08:21 +0700

    create config icon

---
+++ src/config/icons/SvgQr.jsx
@@ -0,0 +1,11 @@
+const SvgQr = () => {
+    return (
+         <div className="cursor-pointer duration-200 py-5 px-2">
+             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" className="d7ed-SwZDZ2">
+            <path d="M9 2a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h5zm0 2H4v5h5V4zm11-2a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-5a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h5zm0 2h-5v5h5V4zM9 13a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h5zm0 2H4v5h5v-5zm8.5-2a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9zm0 2a2.5 2.5 0 0 0 0 5 2.5 2.5 0 0 0 0-5z" fill="#FFFFFF" fillRule="nonzero"></path>
+        </svg>
+         </div>
+    );
+};
+
+export default SvgQr;
