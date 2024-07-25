202ba8fe6b6ba64741369066ca01d41255c73427
thangnt15 <101080448+thangnt15@users.noreply.github.com>
2024-07-25 22:24:38 +0700

    Add files via upload

---
+++ src/contents/Detail/Comment.jsx
@@ -0,0 +1,12 @@
+
+const Comment = () => {
+    return (
+        <>
+            <div>
+                <span><b>Đánh giá nhận xét về sản phẩm</b> ( 15 lượt đánh giá )</span>
+            </div>
+        </>
+    );
+};
+
+export default Comment;
