549a0eee77041206aea04846b0130c1ad3dca2c1
unknown <quanghack12b@gmail.com>
2024-10-18 21:06:13 +0700

    fix domain

--- src/configs/api.config.js
+++ src/configs/api.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instanceApi = axios.create({
-  baseURL: "https://datn-glitch-server-1.onrender.com/",
+  baseURL: "https://datn-glitch-server-1.onrender.com/api/v1",
   withCredentials: true,
 });
 
